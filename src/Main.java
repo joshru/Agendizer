b1142face004f4a38fb2065a092f507a85e0b617
Josh <joshru@uw.edu>
2015-11-29 21:57:47 -0800

    username and password for mysql added as a comment

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,9 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+//mysql username: _445team2
+//mysql password: poddoif
+
 public class Main extends Application {
 
     @Override
