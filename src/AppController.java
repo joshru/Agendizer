6a336bc689d595dcd82a2d397089704877dd5cdf
Josh Rueschenberg <joshru@uw.edu>
2015-11-30 16:38:16 -0800

    Pushing DEEPAH

---
+++ src/AppController.java
@@ -0,0 +1,44 @@
+import javafx.fxml.FXML;
+import javafx.scene.control.*;
+import javafx.scene.image.ImageView;
+
+/**
+ * Created by Josh Rueschenberg on 11/30/2015.
+ */
+public class AppController {
+
+    @FXML private ImageView appLogo;
+    @FXML private Button searchButton;
+    @FXML private TextField searchField;
+    @FXML private TabPane tabContainer;
+    @FXML private Tab upcomingTab;
+    @FXML private Tab completedTab;
+    @FXML private Tab newTaskTab;
+    @FXML private Button upcomingNewTaskButton;
+    @FXML private Button completedNewTaskButton;
+    @FXML private TableView upcomingTaskTable;
+    @FXML private TableView completedTaskTable;
+
+    // these columns should probably be in an array or something. messy
+    @FXML private TableColumn ucTaskCol;
+    @FXML private TableColumn ucDeadlineCol;
+    @FXML private TableColumn ucDifficultyCol;
+    @FXML private TableColumn ucUrgencyCol;
+    @FXML private TableColumn ucPriorityCol;
+    @FXML private TableColumn compTaskCol;
+    @FXML private TableColumn compDeadlineCol;
+    @FXML private TableColumn compDifficultyCol;
+    @FXML private TableColumn compUrgencyCol;
+    @FXML private TableColumn compPriorityCol;
+
+    @FXML private TextField newTaskDescpriton;
+    @FXML private DatePicker newTaskDeadline;
+    @FXML private ChoiceBox newTaskDifficulty;
+    @FXML private ChoiceBox newTaskUrgency;
+    @FXML private ChoiceBox newTaskPriority;
+
+    @FXML
+    private void test() {
+        System.out.println("button did a thing");
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,17 @@
 import javafx.application.Application;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.fxml.JavaFXBuilderFactory;
+import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.*;
 import javafx.scene.image.ImageView;
 import javafx.stage.Stage;
+
+import javax.swing.*;
+import java.io.IOException;
 
 //mysql username: _445team2
 //mysql password: poddoif
@@ -14,37 +20,7 @@ public class Main extends Application {
 
     private static boolean DEBUG = true;
 
-    @FXML private ImageView appLogo;
-    @FXML private Button searchButton;
-    @FXML private TextField searchField;
-    @FXML private TabPane tabContainer;
-    @FXML private Tab upcomingTab;
-    @FXML private Tab completedTab;
-    @FXML private Tab newTaskTab;
-    @FXML private Button upcomingNewTaskButton;
-    @FXML private Button completedNewTaskButton;
-    @FXML private TableView upcomingTaskTable;
-    @FXML private TableView completedTaskTable;
 
-    // these columns should probably be in an array or something. messy
-    @FXML private TableColumn ucTaskCol;
-    @FXML private TableColumn ucDeadlineCol;
-    @FXML private TableColumn ucDifficultyCol;
-    @FXML private TableColumn ucUrgencyCol;
-    @FXML private TableColumn ucPriorityCol;
-    @FXML private TableColumn compTaskCol;
-    @FXML private TableColumn compDeadlineCol;
-    @FXML private TableColumn compDifficultyCol;
-    @FXML private TableColumn compUrgencyCol;
-    @FXML private TableColumn compPriorityCol;
-
-    @FXML private TextField newTaskDescpriton;
-    @FXML private DatePicker newTaskDeadline;
-    @FXML private ChoiceBox newTaskDifficulty;
-    @FXML private ChoiceBox newTaskUrgency;
-    @FXML private ChoiceBox newTaskPriority;
-
-    @FXML private Button loginButton;
 
     private Stage myPrimaryStage;
 
@@ -53,12 +29,15 @@ public class Main extends Application {
 //        Parent gui = FXMLLoader.load(getClass().getResource("gui.fxml"));
 
         myPrimaryStage = primaryStage;
+//        switchToLogin();
+//        primaryStage.show();
+
 
         Parent login = FXMLLoader.load(getClass().getResource("login.fxml"));
 //        login.get
-        myPrimaryStage.setTitle("Please Log In To Agendizer");
-        myPrimaryStage.setScene(new Scene(login, 1000, 700));
-        myPrimaryStage.show();
+        primaryStage.setTitle("Please Log In To Agendizer");
+        primaryStage.setScene(new Scene(login, 1000, 700));
+        primaryStage.show();
     }
 
 
@@ -72,11 +51,37 @@ public class Main extends Application {
 
     public void switchToGUI() throws Exception {
         if (DEBUG) System.out.println("login button pressed");
-
-        Parent gui = FXMLLoader.load(getClass().getResource("gui.fxml"));
+        replaceSceneContent("gui.fxml");
+//
+//        Parent gui = FXMLLoader.load(getClass().getResource("gui.fxml"));
 //        myPrimaryStage.setTitle("Agendizer");
-        myPrimaryStage.setScene(new Scene(gui, 1000, 700));
+//        myPrimaryStage.setScene(new Scene(gui, 1000, 700));
 //        primaryStage.show();
     }
+
+    public void switchToLogin() throws Exception {
+        replaceSceneContent("login.fxml");
+    }
+
+    @FXML
+    public Parent replaceSceneContent(String fxml) throws Exception {
+//        FXMLLoader loader = new FXMLLoader(getClass().getResource(fxml));
+//        Parent root = loader.load();
+//        Scene scene = new Scene(root, 1000, 700);
+//        myPrimaryStage.setScene(scene);
+        Parent page = FXMLLoader.load(getClass().getResource(fxml));
+        Scene scene = myPrimaryStage.getScene();
+        if (scene == null) {
+            scene = new Scene(page, 1000, 700);
+            myPrimaryStage.setScene(scene);
+            myPrimaryStage.show();
+        } else {
+            myPrimaryStage.getScene().setRoot(page);
+        }
+        myPrimaryStage.sizeToScene();
+        return page;
+    }
+
+
 
 }
