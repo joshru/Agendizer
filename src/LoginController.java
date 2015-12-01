18c6ffa5d4e217146fede49424d7cf3df85709b8
Josh Rueschenberg <joshru@uw.edu>
2015-11-30 18:18:52 -0800

    Registration page is almost done

--- src/LoginController.java
+++ src/LoginController.java
@@ -5,7 +5,11 @@ import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
 import javafx.scene.input.MouseEvent;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -17,19 +21,47 @@ import java.util.ResourceBundle;
  */
 public class LoginController implements Initializable {
 
+    @FXML private Label loginError;
+    @FXML private TextField usernameLoginField;
+    @FXML private PasswordField passwordLoginField;
     @FXML private Button loginButton;
+    @FXML private Text newUserButton;
+    @FXML private TextField firstNameField;
+    @FXML private TextField lastNameField;
+    @FXML private TextField usernameRegisterField;
+    @FXML private TextField emailField;
+    @FXML private TextField passwordRegisterField;
+    @FXML private TextField passwordConfirmation;
+    @FXML private Button createAccountButton;
+    @FXML private Button returnToLoginButton;
+    @FXML private Label registerWarningLabel;
+
 
 
     @FXML
     private void handleLoginButton(MouseEvent event) throws IOException {
-        Parent home_page_parent = FXMLLoader.load(getClass().getResource("view/gui.fxml"));
-        Scene home_page_scene = new Scene(home_page_parent, 1000, 700);
-        Stage app_stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        swapScene("view/gui.fxml", "Agendizer", event);
+    }
 
-        app_stage.setScene(home_page_scene);
-        app_stage.setTitle("Agendizer");
-        app_stage.show();
+    @FXML
+    private void handleRegisterButton(MouseEvent event) throws IOException {
+        swapScene("view/register.fxml", "Agendizer Registration", event);
+    }
 
+    @FXML
+    private void handleReturnToLogin(MouseEvent event) throws IOException {
+        swapScene("view/login.fxml", "Please Log In To Agendizer", event);
+    }
+
+    @FXML
+    private void swapScene(final String fxml, final String title, MouseEvent event) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource(fxml));
+        Scene scene = new Scene(root, 1000, 700);
+        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+
+        stage.setScene(scene);
+        stage.setTitle(title);
+        stage.show();
     }
 
     @Override
