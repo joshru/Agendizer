fd7f64434b0fb15e813223caa85eabeaaf62d930
Josh <joshru@uw.edu>
2015-12-01 21:25:51 -0800

    Working on making new tasks from GUI

--- src/AppController.java
+++ src/AppController.java
@@ -1,9 +1,16 @@
 import Databases.TaskDB;
+import Model.Task;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.scene.control.*;
 import javafx.scene.image.ImageView;
 
 import java.sql.SQLException;
+import java.time.LocalDate;
+import java.util.Date;
+import java.util.List;
+import java.util.UUID;
 
 /**
  * Created by Josh Rueschenberg on 11/30/2015.
@@ -39,8 +46,16 @@ public class AppController {
     @FXML private ChoiceBox newTaskDifficulty;
     @FXML private ChoiceBox newTaskUrgency;
     @FXML private ChoiceBox newTaskPriority;
+    @FXML private Button newTaskButton;
 
     private TaskDB db = new TaskDB();
+
+//    ObservableList<String> difficultyVals = FXCollections.observableArrayList("one", "two");
+
+    @FXML
+    private void newTaskTabSetup() {
+//        newTaskDifficulty.setItems(difficultyVals);
+    }
 
     @FXML
     private void test() {
@@ -49,6 +64,29 @@ public class AppController {
 
     @FXML
     private void testDB() throws SQLException {
-        db.getTasks();
+        List<Task> tasks = db.getTasks();
+        System.out.println("Tasks grabbed = " +  tasks.size());
     }
+
+
+    //https://community.oracle.com/thread/2486012?tstart=0
+    // helpful link about combo boxes
+    @FXML
+    private void getTaskDetails() {
+        String taskName = newTaskDescpriton.getText();
+        String taskID = " " +  UUID.randomUUID(); //hella sneaky sneaks
+        LocalDate timestamp = newTaskDeadline.getValue();
+        int completed = 0;
+        String category = ""; //TODO so much random shit in a task
+        String difficulty = newTaskDifficulty.getSelectionModel().getSelectedItem().toString();
+        System.out.println(difficulty);
+        String urgency = newTaskUrgency.getSelectionModel().getSelectedItem().toString();
+        System.out.println(urgency);
+        String priority = newTaskPriority.getSelectionModel().getSelectedItem().toString();
+        System.out.println(priority);
+
+
+    }
+
+
 }
